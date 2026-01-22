32ce50362c609f067522d1752d7460512cae1789
unknown <trandangchien23082005@gmail.com>
2026-01-22 07:48:23 +0700

    luong thong ke luong va hop dong

--- F_BaoCaoLuong.cs
+++ F_BaoCaoLuong.cs
@@ -12,7 +12,7 @@ using ClosedXML.Excel;
 
 namespace QuanLyNhanVien3
 {
-    public partial class F_BaoCaoLuong: Form
+    public partial class F_BaoCaoLuong : Form
     {
         public F_BaoCaoLuong()
         {
@@ -20,16 +20,6 @@ namespace QuanLyNhanVien3
         }
 
         connectData cn = new connectData();
-        private void ClearAllInputs(Control parent)
-        {
-            foreach (Control ctl in parent.Controls)
-            {
-                if (ctl is TextBox) ((TextBox)ctl).Clear();
-                else if (ctl is ComboBox) ((ComboBox)ctl).SelectedIndex = -1;
-                // Không reset DateTimePicker về Now để người dùng tiện thao tác tiếp
-                else if (ctl.HasChildren) ClearAllInputs(ctl);
-            }
-        }
 
         private void btnLuongHangThang_Click(object sender, EventArgs e)
         {
@@ -37,27 +27,27 @@ namespace QuanLyNhanVien3
             {
                 cn.connect();
 
-                // Lấy tháng và năm từ DateTimePicker trên giao diện
-                // Giả sử tên control là dtpThoiGian
                 int thang = dateTimePicker1.Value.Month;
                 int nam = dateTimePicker1.Value.Year;
 
-                string sql = @"SELECT l.MaLuong AS N'Mã Lương', 
-                                      nv.MaNV AS N'Mã Nhân Viên', 
-                                      nv.HoTen AS N'Họ Tên', 
-                                      l.Thang AS N'Tháng', 
-                                      l.Nam AS N'Năm', 
-                                      l.LuongCoBan AS N'Lương Cơ Bản', 
-                                      l.SoNgayCong AS N'Ngày Công',
-                                      l.PhuCap AS N'Phụ Cấp',
-                                      l.KhauTru AS N'Khấu Trừ',
-                                      l.TongLuong AS N'Tổng Lương'
-                               FROM tblLuong l
-                               JOIN tblNhanVien nv ON l.MaNV = nv.MaNV
-                               WHERE l.DeletedAt = 0 
-                               AND l.Thang = @Thang 
-                               AND l.Nam = @Nam
-                               ORDER BY l.TongLuong DESC";
+                // Sửa lại query theo đúng cấu trúc CSDL
+                string sql = @"SELECT l.MaLuong_ChienCD232928 AS N'Mã Lương', 
+                                      nv.MaNV_TuanhCD233018 AS N'Mã Nhân Viên', 
+                                      nv.HoTen_TuanhCD233018 AS N'Họ Tên', 
+                                      l.Thang_ChienCD232928 AS N'Tháng', 
+                                      l.Nam_ChienCD232928 AS N'Năm', 
+                                      l.LuongCoBan_ChienCD232928 AS N'Lương Cơ Bản', 
+                                      l.SoNgayCongChuan_ChienCD232928 AS N'Ngày Công Chuẩn',
+                                      l.PhuCap_ChienCD232928 AS N'Phụ Cấp',
+                                      l.KhauTru_ChienCD232928 AS N'Khấu Trừ',
+                                      (l.LuongCoBan_ChienCD232928 + l.PhuCap_ChienCD232928 - l.KhauTru_ChienCD232928) AS N'Tổng Lương'
+                               FROM tblLuong_ChienCD232928 l
+                               JOIN tblChamCong_TuanhCD233018 cc ON l.ChamCongId_TuanhCD233018 = cc.Id_TuanhCD233018
+                               JOIN tblNhanVien_TuanhCD233018 nv ON cc.NhanVienId_TuanhCD233018 = nv.Id_TuanhCD233018
+                               WHERE l.DeletedAt_ChienCD232928 = 0 
+                               AND l.Thang_ChienCD232928 = @Thang 
+                               AND l.Nam_ChienCD232928 = @Nam
+                               ORDER BY N'Tổng Lương' DESC";
 
                 using (SqlCommand cmd = new SqlCommand(sql, cn.conn))
                 {
@@ -68,7 +58,6 @@ namespace QuanLyNhanVien3
                     DataTable dt = new DataTable();
                     adapter.Fill(dt);
 
-                    // Giả sử tên GridView là dtGridViewBCLuong
                     dtGridViewBCLuong.DataSource = dt;
                 }
 
@@ -88,20 +77,17 @@ namespace QuanLyNhanVien3
                 int thang = dateTimePicker1.Value.Month;
                 int nam = dateTimePicker1.Value.Year;
 
-                // Lấy TOP 1 nhân viên lương cao nhất (WITH TIES để lấy nhiều người nếu bằng lương nhau)
                 string sql = @"SELECT TOP 1 WITH TIES 
-                                      nv.HoTen AS N'Họ Tên', 
-                                      pb.TenPB AS N'Phòng Ban',
-                                      cv.TenCV AS N'Chức Vụ',
-                                      l.TongLuong AS N'Tổng Lương Cao Nhất'
-                               FROM tblLuong l
-                               JOIN tblNhanVien nv ON l.MaNV = nv.MaNV
-                               JOIN tblPhongBan pb ON nv.MaPB = pb.MaPB
-                               JOIN tblChucVu cv ON nv.MaCV = cv.MaCV
-                               WHERE l.DeletedAt = 0 
-                               AND l.Thang = @Thang 
-                               AND l.Nam = @Nam
-                               ORDER BY l.TongLuong DESC";
+                                      nv.HoTen_TuanhCD233018 AS N'Họ Tên', 
+                                      nv.MaNV_TuanhCD233018 AS N'Mã Nhân Viên',
+                                      (l.LuongCoBan_ChienCD232928 + l.PhuCap_ChienCD232928 - l.KhauTru_ChienCD232928) AS N'Tổng Lương Cao Nhất'
+                               FROM tblLuong_ChienCD232928 l
+                               JOIN tblChamCong_TuanhCD233018 cc ON l.ChamCongId_TuanhCD233018 = cc.Id_TuanhCD233018
+                               JOIN tblNhanVien_TuanhCD233018 nv ON cc.NhanVienId_TuanhCD233018 = nv.Id_TuanhCD233018
+                               WHERE l.DeletedAt_ChienCD232928 = 0 
+                               AND l.Thang_ChienCD232928 = @Thang 
+                               AND l.Nam_ChienCD232928 = @Nam
+                               ORDER BY N'Tổng Lương Cao Nhất' DESC";
 
                 using (SqlCommand cmd = new SqlCommand(sql, cn.conn))
                 {
@@ -129,17 +115,18 @@ namespace QuanLyNhanVien3
                 int thang = dateTimePicker1.Value.Month;
                 int nam = dateTimePicker1.Value.Year;
 
-                // Group by theo tên phòng ban và tính tổng lương
-                string sql = @"SELECT pb.TenPB AS N'Tên Phòng Ban', 
-                                      COUNT(l.MaNV) AS N'Số Nhân Viên Đã Nhận Lương',
-                                      SUM(l.TongLuong) AS N'Tổng Quỹ Lương'
-                               FROM tblLuong l
-                               JOIN tblNhanVien nv ON l.MaNV = nv.MaNV
-                               JOIN tblPhongBan pb ON nv.MaPB = pb.MaPB
-                               WHERE l.DeletedAt = 0 
-                               AND l.Thang = @Thang 
-                               AND l.Nam = @Nam
-                               GROUP BY pb.TenPB";
+                // Vì bảng nhân viên không có cột phòng ban, ta sẽ bỏ phần này hoặc tìm cách khác
+                // Tạm thời chỉ hiển thị tổng lương toàn công ty
+                string sql = @"SELECT 
+                                      'Toàn Công Ty' AS N'Phòng Ban', 
+                                      COUNT(DISTINCT nv.Id_TuanhCD233018) AS N'Số Nhân Viên Đã Nhận Lương',
+                                      SUM(l.LuongCoBan_ChienCD232928 + l.PhuCap_ChienCD232928 - l.KhauTru_ChienCD232928) AS N'Tổng Quỹ Lương'
+                               FROM tblLuong_ChienCD232928 l
+                               JOIN tblChamCong_TuanhCD233018 cc ON l.ChamCongId_TuanhCD233018 = cc.Id_TuanhCD233018
+                               JOIN tblNhanVien_TuanhCD233018 nv ON cc.NhanVienId_TuanhCD233018 = nv.Id_TuanhCD233018
+                               WHERE l.DeletedAt_ChienCD232928 = 0 
+                               AND l.Thang_ChienCD232928 = @Thang 
+                               AND l.Nam_ChienCD232928 = @Nam";
 
                 using (SqlCommand cmd = new SqlCommand(sql, cn.conn))
                 {
@@ -246,9 +233,15 @@ namespace QuanLyNhanVien3
                     MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
-    
 
-        
+        private void ClearAllInputs(Control parent)
+        {
+            foreach (Control ctl in parent.Controls)
+            {
+                if (ctl is TextBox) ((TextBox)ctl).Clear();
+                else if (ctl is ComboBox) ((ComboBox)ctl).SelectedIndex = -1;
+                else if (ctl.HasChildren) ClearAllInputs(ctl);
+            }
+        }
     }
-
 }
